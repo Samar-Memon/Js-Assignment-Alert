daa7ff7e7c57052d01f32760e1e95a3b59a283c6
Samar-Memon <samar56786raza@gmail.com>
2024-03-09 22:25:19 +0500

    Js-Assignment1-pt-1

---
+++ alert.js
@@ -0,0 +1,9 @@
+alert('Error! Please enter a valid password');
+
+alert('Welcome to JS');
+
+alert('Welcome to JS Land');
+
+alert('Prevent this Page from creating additional dialogs.');
+
+alert('Hello I Can run JS through my web browsers console');
